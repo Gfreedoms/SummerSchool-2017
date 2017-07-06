a365db9ee95ee44fa8220193e2cf050942257093
Gfreedoms <freedomgemmar1@gmail.com>
2017-07-06 19:09:39 +0300

    Add files via upload

---
+++ Upload.js
@@ -0,0 +1,16 @@
+var add = function(a,b){
+    return a+b;
+}
+var subtract = function(c,d){
+    return c-d;
+}
+var multiply = function(e,f){
+    return e*f;
+}
+var devide = function(g,h){
+    return g/h;
+}
+console.log(add(2,3));
+console.log(subtract(5,1));
+console.log(multiply(7,3));
+console.log(devide(8,4));
