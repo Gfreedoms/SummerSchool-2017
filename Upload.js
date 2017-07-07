2f65a322a9a047e30c78755d19db74906aa698e4
Gfreedoms <freedomgemmar1@gmail.com>
2017-07-07 13:57:14 +0300

    Array summing elements

--- Upload.js
+++ Upload.js
@@ -14,3 +14,10 @@ console.log(add(2,3));
 console.log(subtract(5,1));
 console.log(multiply(7,3));
 console.log(devide(8,4));
+
+var one = [2, 5, 6, 7, 7, 65,];
+
+var total = one.reduce(function(sum,value){
+    return sum + value;
+}, 0);
+console.log( total );
