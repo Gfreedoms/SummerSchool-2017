7b69c504f4fe47e4a492bb6ac961166f5a96c720
SamuelMuloki <samuellebron0@gmail.com>
2017-07-07 13:57:52 +0300

    Worked on Difference

--- Upload.js
+++ Upload.js
@@ -1,16 +1,25 @@
 var add = function(a,b){
-    return a+b;
+    return a + b;
 }
 var subtract = function(c,d){
-    return c-d;
+    return c - d;
 }
 var multiply = function(e,f){
-    return e*f;
+    return e * f;
 }
-var devide = function(g,h){
-    return g/h;
+var divide = function(g,h){
+    return g / h;
 }
 console.log(add(2,3));
 console.log(subtract(5,1));
 console.log(multiply(7,3));
-console.log(devide(8,4));
+console.log(divide(8,4));
+
+var Array1 = [337, 67, 7];
+var diff;
+    for(var x = 0; x < Array1.length-1; x++){
+       diff = Array1[x] - Array1[x+1];
+       console.log(diff);
+    }
+    
+console.log( diff );
